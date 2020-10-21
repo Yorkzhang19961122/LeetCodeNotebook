dfcba23f3377f5dca06a071fece85b5e1d9401ad
zyk <15257379410@163.com>
2020-10-21 15:27:47 +0800

    Update 1512.好数对的数目_E.cpp

--- 数组/1512.好数对的数目_E.cpp
+++ 数组/1512.好数对的数目_E.cpp
@@ -1,3 +1,4 @@
+todo：使用哈希表
 /*
 【题目】
 给你一个整数数组 nums，如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对，返回好数对的数目。
@@ -8,9 +9,10 @@
 */
 
 /*
-思路：暴力法，两重循环遍历数组nums，
+思路：暴力法，使用两重循环遍历数组nums，再根据好数对的条件进行判断。
 */
 
+/*代码*/
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
