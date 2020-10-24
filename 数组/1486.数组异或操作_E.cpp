463f570aee68a6ba1a2a00a23c3c177bb45a5a26
zyk <15257379410@163.com>
2020-10-24 13:14:29 +0800

    Create 1486.数组异或操作_E.cpp

---
+++ 数组/1486.数组异或操作_E.cpp
@@ -0,0 +1,45 @@
+/*
+【题目】
+给你两个整数，n 和 start 。
+数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+【示例】
+输入：n = 5, start = 0
+输出：8
+解释：数组 nums 为 [0, 2, 4, 6, 8]，其中 (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8 。
+     "^" 为按位异或 XOR 运算符。
+*/
+
+/*
+我的思路：按照题意不难构造出数组nums，再将nums按位异或即可
+*/
+
+/*我的代码*/
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums(n,0);  //初始化nums为n个0
+        int m = 0;
+        for(int i = 0; i < n; i++)  //据题意得到nums
+            nums[i] = start + 2*i;
+        for(int j = 0; j < n; j++)  //按位异或
+            m ^= nums[j];
+        return m;
+    }
+};
+
+/*
+题解思路：思路与我类似，简化代码，不再构建数组nums
+*/
+
+/*题解代码*/
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int res = 0;  //初始化res用于存储结果
+        for(int i = 0; i < n; i++){
+            res ^= (start + 2*i);  //按位异或
+        }
+        return res;
+    }
+};
